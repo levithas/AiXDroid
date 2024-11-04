41b2002caf10710d046daa756de86cc3cb670f0a
Steffen Wolf <s.wolf@fh-aachen.de>
2024-11-04 10:02:02 +0100

    Add Conflict strategy for Insertion

--- app/src/main/java/de/levithas/aixdroid/data/dao/AIModelDao.kt
+++ app/src/main/java/de/levithas/aixdroid/data/dao/AIModelDao.kt
@@ -3,13 +3,14 @@ package de.levithas.aixdroid.data.dao
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import de.levithas.aixdroid.data.model.AIModelConfigurationEntity
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface AIModelDao {
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertModel(model: AIModelConfigurationEntity)
 
     @Query("SELECT * FROM AIModelConfiguration WHERE id == :modelId")
